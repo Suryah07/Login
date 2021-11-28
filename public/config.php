88a53d9feb52585e2bb0a7b4f14704ed89d5bb6d
Suryah07 <suryahdhamodharan@gmail.com>
2021-11-28 10:48:58 +0530

    Login successful

---
+++ public/config.php
@@ -0,0 +1,10 @@
+<?php 
+
+define("dbserver","localhost");
+define("dbuser","root");
+define("dbpass","root1234");
+define("dbname","login_project");
+
+$conn = mysqli_connect(dbserver,dbuser,dbpass,dbname);
+
+?>
---
+++ public/login.php
@@ -0,0 +1,39 @@
+<?php
+include("config.php");
+$uname = $_POST['username'];
+$upass = $_POST['password'];
+//$unumber = $_POST['phone'];
+//$umessage = $_POST['message'];
+
+// Check connection 
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  $sql = "SELECT * FROM userinfo";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) 
+  {
+    while($row = $result->fetch_assoc()) 
+    {
+      //echo "id: " . $row["name"]. " - Name: " . $row["pass"]."<br>";
+      $dname = $row["name"];
+      $dpass = $row["pass"];
+      if($dname == $uname)
+      {
+        if($dpass == $upass)
+        {
+          echo "login successfull";
+        }
+      }
+    }
+  }
+  else 
+  {
+    echo "0 results";
+  }
+
+  //echo "Connection established ".$uname."welcome".$upass;
+  
+  $conn->close();
+  ?>
